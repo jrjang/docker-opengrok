03f02e1f932ae83878ccae4e290fb15cf00eaeab
Jun-Ru Chang <jrjang@gmail.com>
2017-01-24 18:22:07 +0800

    update opengrok version
    Signed-off-by: Jun-Ru Chang <jrjang@gmail.com>

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-wget -O - https://java.net/projects/opengrok/downloads/download/opengrok-0.12.1.5.tar.gz | tar zxvf -
+wget -O - https://github.com/OpenGrok/OpenGrok/files/551372/opengrok-0.13-rc4.tar.gz | tar zxvf -
 mv opengrok-* opengrok
 cd /etc/tomcat7 && patch -p1 < 0001-tomcat-increase-http-header-size-to-65536.patch
